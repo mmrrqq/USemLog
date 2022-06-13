747526969000944cb4908f48a62246f1f0329a65
Abhijit Vyas <vyasabhijit21@gmail.com>
2022-06-13 20:24:11 +0200

    updates related to sl particles to log into semmantic world owl

--- Source/USemLog/Classes/Individuals/Type/SLParticleIndividual.h
+++ Source/USemLog/Classes/Individuals/Type/SLParticleIndividual.h
@@ -18,17 +18,33 @@ public:
     // Ctor
 	USLParticleIndividual();
 
-     
+    // Called before destroying the object.
+    virtual void BeginDestroy() override;
+
+    // Init asset references (bForced forces re-initialization)
+    virtual bool Init(bool bReset);
+
+    // Load semantic data (bForced forces re-loading)
+    virtual bool Load(bool bReset, bool bTryImport);
+
+    // Get the type name as string
+    virtual FString GetTypeName() const override { return FString("ParticleIndividual"); };
+
 protected:
-	// Update its location according to the gaze data
-	void Update();
-
-	// Listen if it can listen to gaze data
-	void Init();
-
-	// True is it can
-	bool bIsInit;
+    // Get class name, virtual since each invidiual type will have different name
+    virtual FString CalcDefaultClassValue() override;
  
-      
+private:
+    // Set dependencies
+    bool InitImpl();
+
+    // Set data
+    bool LoadImpl(bool bTryImport);
+
+    // Clear all values of the individual
+    void InitReset();
+
+    // Clear all data of the individual
+    void LoadReset();
 };
 
--- Source/USemLog/Private/Individuals/Type/SLParticleIndividual.cpp
+++ Source/USemLog/Private/Individuals/Type/SLParticleIndividual.cpp
@@ -5,20 +5,83 @@ USLParticleIndividual::USLParticleIndividual() {
 }
 
  
-
-// Connect to the gaze data
-void USLParticleIndividual::Init()
+// Called before destroying the object.
+void USLParticleIndividual::BeginDestroy()
 {
-	if (bIsInit)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("%s::%d %s is already initialized.."), *FString(__FUNCTION__), __LINE__, *GetName());
-		return;
-	}
-	 
-	 
+	SetIsInit(false);
+	Super::BeginDestroy();
 }
 
-// Update pose according to the gaze data
-void USLParticleIndividual::Update()
-{ 
+// Set pointer to the semantic owner
+bool USLParticleIndividual::Init(bool bReset)
+{
+	if (bReset)
+	{
+		InitReset();
+	}
+
+	if (IsInit())
+	{
+		return true;
+	}
+
+	SetIsInit(Super::Init(bReset) && InitImpl());
+	return IsInit();
+}
+
+// Load semantic data
+bool USLParticleIndividual::Load(bool bReset, bool bTryImport)
+{
+	if (bReset)
+	{
+		LoadReset();
+	}
+
+	if (IsLoaded())
+	{
+		return true;
+	}
+
+	if (!IsInit())
+	{
+		if (!Init(bReset))
+		{
+			UE_LOG(LogTemp, Log, TEXT("%s::%d Cannot load individual %s, init fails.."),
+				*FString(__FUNCTION__), __LINE__, *GetFullName());
+			return false;
+		}
+	}
+
+	SetIsLoaded(Super::Load(bReset, bTryImport) && LoadImpl(bTryImport));
+	return IsLoaded();
+}
+
+// Get class name, virtual since each invidiual type will have different name
+FString USLParticleIndividual::CalcDefaultClassValue()
+{
+	return GetTypeName();
+}
+
+// Private init implementation
+bool USLParticleIndividual::InitImpl()
+{
+	return true;
+}
+
+// Private load implementation
+bool USLParticleIndividual::LoadImpl(bool bTryImport)
+{
+	return true;
+}
+
+// Clear all values of the individual
+void USLParticleIndividual::InitReset()
+{
+	SetIsInit(false);
+}
+
+// Clear all data of the individual
+void USLParticleIndividual::LoadReset()
+{
+	SetIsLoaded(false);
 }
